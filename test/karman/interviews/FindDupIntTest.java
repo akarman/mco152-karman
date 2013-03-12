6cfff5ac67bc7531ba4d853cd9809731bca70fa8
Zavie <Zavie@Karman>
2013-03-12 13:49:58 -0400

    Minor changes to FindDupInt test

--- test/karman/interviews/FindDupIntTest.java
+++ test/karman/interviews/FindDupIntTest.java
@@ -13,6 +13,7 @@ public class FindDupIntTest {
 		Assert.assertEquals(8, duplicateFinder.findDup());
 	}
 	
+	@Test
 	public void testNoDup(){
 		int[] numbers= new int[]{0,1,2,3,4,5,6,7,8,9};
 		FindDupInt duplicateFinder= new FindDupInt(numbers);
